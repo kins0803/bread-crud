96d67bbce1de9bd592d6b8b94e8cdcbf44c9b785
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-05-09 20:48:27 -0400

    set up database and get routes

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,9 +2,10 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 // GET all the bread
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
+    const bread = await Bread.find()
     res.render('index', {
-        breads: Bread
+        breads: bread
     })
 })
 
@@ -13,11 +14,11 @@ router.get('/new', (req, res) => {
 })
 
 // GET bread by specific name
-router.get('/:index', (req, res) => {
-    const { index } = req.params
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const bread = await Bread.findById(id)
     res.render('show', {
-        bread: Bread[index],
-        index
+        bread
     })
 })
 
@@ -29,8 +30,8 @@ router.get('/:index/edit', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
-    if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+router.post('/', async (req, res) => {
+    if (!req.body.image) req.body.image = undefined
 
     if (req.body.hasGluten === 'on') {
         req.body.hasGluten = true
@@ -38,7 +39,7 @@ router.post('/', (req, res) => {
         req.body.hasGluten = false
     }
 
-    Bread.push(req.body)
+    await Bread.create(req.body)
     res.status(303).redirect('/breads')
 })
 
